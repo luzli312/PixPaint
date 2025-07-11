8435dc7b88dc01e3f63ea454a16af4bb0888b721
luzli312 <lisabethcl3@gmail.com>
2025-07-11 19:43:49 -0400

    Create QuickStart.java

---
+++ src/main/java/QuickStart.java
@@ -0,0 +1,28 @@
+import static com.mongodb.client.model.Filters.eq;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class QuickStart {
+    public static void main( String[] args ) {
+
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://luzli:sxpLqxjD4N6NuEyW@cluster0.xvafcwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+
+            Document doc = collection.find(eq("title", "The Four Horsemen of the Apocalypse")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+    }
+}
