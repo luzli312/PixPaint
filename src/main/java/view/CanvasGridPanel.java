e7720c8d6896ead5beb09481199f25c42c9b3b54
emily <emily.escalante@mail.utoronto.ca>
2025-07-18 17:43:22 -0600

    Canvas grid with buttons for squares

---
+++ src/main/java/view/CanvasGridPanel.java
@@ -0,0 +1,32 @@
+package view;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CanvasGridPanel extends JPanel {
+    // Create 32 x 32 grid, white squares, maybe add black lines to separate each square.
+    // JPanel
+    private static final int LENGTH = 32;
+    private static final int SIDE_LENGTH = 20;
+    private static final int GAP = 1;
+    private static final Color BORDER = Color.decode("#000000");
+    private static final Color DEFAULT = Color.decode("#FFFFFF");
+
+    public CanvasGridPanel()
+    {
+        setBackground(BORDER);
+        setBorder(BorderFactory.createEmptyBorder(GAP, GAP, GAP, GAP));
+        setLayout(new GridLayout(LENGTH, LENGTH, GAP, GAP));
+        Dimension prefSize = new Dimension(SIDE_LENGTH, SIDE_LENGTH);
+        for (int i = 0; i < LENGTH; i++) {
+            for (int j = 0; j < LENGTH; j++) {
+                JButton cell = new JButton();
+                cell.setBackground(DEFAULT);
+                cell.setPreferredSize(prefSize);
+                add(cell);
+            }
+        }
+    }
+}
+
