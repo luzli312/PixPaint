a30f5d1be6890cf2fd694848c4c486b708135844
katfishy <166878958+katfishy@users.noreply.github.com>
2025-07-31 17:08:22 -0400

    Add functionality to save button

--- src/main/java/usecase/save_canvas/SaveCanvasInteractor.java
+++ src/main/java/usecase/save_canvas/SaveCanvasInteractor.java
@@ -3,12 +3,15 @@ package usecase.save_canvas;
 import data_access.UserDataAccessObject;
 import entity.CanvasData;
 import interface_adapter.logged_in.LoggedInState;
+import entity.User;
 import view.CanvasGridPanel;
+
+import java.awt.*;
 
 public class SaveCanvasInteractor {
 
-    public void execute(CanvasGridPanel canvasGridPanel) {
-        CanvasData test = new CanvasData(LoggedInState.getCurrentUser(), "test", canvasGridPanel);
-        new UserDataAccessObject().createProject(test.exportCanvasData());
+    public void execute(User user, CanvasGridPanel canvasGridPanel, String title) {
+        CanvasData canvasData = new CanvasData(user, title, canvasGridPanel);
+        new UserDataAccessObject().createProject(canvasData.exportCanvasData());
     }
 }
